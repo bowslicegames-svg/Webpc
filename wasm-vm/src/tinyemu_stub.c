c5b10f2cf7bbbaea11e042037d4a26221c4ad961
bowslicegames-svg <bowslicegames@gmail.com>
2026-09-09 13:41:15 +0100

    Delete wasm-vm/src/tinyemu_stub.c

--- wasm-vm/src/tinyemu_stub.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main(int argc, char** argv) {
-    printf("tinyemu_stub: This is a placeholder VM binary.\n");
-    printf("Replace with a real WASM-capable emulator build.\n");
-    return 0;
-}
