562557b49e4b3759904e19d3d46fec73e066e6c0
bowslicegames-svg <bowslicegames@gmail.com>
2026-09-09 11:31:41 +0100

    Add placeholder VM binary for tinyemu_stub

---
+++ wasm-vm/src/tinyemu_stub.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+    printf("tinyemu_stub: This is a placeholder VM binary.\n");
+    printf("Replace with a real WASM-capable emulator build.\n");
+    return 0;
+}
